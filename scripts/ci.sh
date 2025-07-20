2257429effee87d7839ba1c62def7211f82eb0c2
guzio <star.master.123.mg@gmail.com>
2025-07-20 22:58:16 +0200

    CI finished
    I don't know.
    
    Maybe.
    
    I can't test it without pushing.

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -3,20 +3,68 @@
 # THIS IS SUPPOSED TO ONLY BE RUN FROM A DOCKER IMAGE!
 
 echo "-----------STARTED ON: $(date)-----------";
-echo "This is ATM just a simple test whether Docker works!!!";
+
 if [ -z "$REPO" ]; then
     echo "ERROR: You must specify a \$REPO envar!";
     exit 1
 fi
-echo "Uses repo: $REPO";
-echo "Running from: $(pwd) - with contents:";
-ls -al;
-echo "Grinch's help outupt:";
-/app/grinch
-echo "...and Git's:";
-git help
-echo "......and make_serverpacks's (should complain about a missing name):";
-/app/grinch-serverpack
-echo "Test completed. This command will soon exit and (depending on your setup) might be restarted by Docker.";
-sleep 5;
-exit 0;
+if [ -z "$NAME" ]; then
+    echo "ERROR: You must specify a \$NAME envar!";
+    exit 1
+fi
+echo "Using repo: $REPO";
+
+if [ -e "cache" ]; then
+    echo "...which is already cached - will do a simple git pull to check for updates instead of a full git clone.";
+    cd "cache" || exit
+    git pull || exit
+else
+    echo "...which has to be cloned.";
+    git clone "$REPO" "cache" || exit
+    echo "[STARTING FROM SCRATCH]" >> "last-version.txt"
+fi
+
+cd "cache" || exit
+
+if [ "$(/app/grinch vq)" = "$(cat last-version.txt)" ]; then
+    echo "You seem to be using an up-to date modpack. Waiting for 15s until the next cycle.";
+    sleep 15
+    exec "/app/ci.sh";
+    echo "ERROR: If you're seeing this, the next cycle couldn't be started!";
+    exit 1;
+fi
+
+echo "Building modpack version $(/app/grinch vq)";
+
+MRP=".mrpack"
+Q="quick$MRP"
+S="slim$MRP"
+T="tweakable$MRP"
+/app/grinch e -qT "$Q" || exit
+/app/grinch e -sT "$S" || exit
+/app/grinch e -tT "$T" || exit
+/app/grinch-serverpack "$Q" || exit
+
+EXPORTNAME="$NAME $(/app/grinch vq)";
+echo "Exporting just-built assets as $EXPORTNAME";
+SERVERPACK="$EXPORTNAME - Server Edition$MRP"
+mv "$Q" "$EXPORTNAME$MRP" || exit
+mv "$S" "$EXPORTNAME - Slim Edition$MRP" || exit
+mv "$T" "$EXPORTNAME - Tweakable Edition$MRP" || exit
+mv "serverpack-$Q" "$SERVERPACK" || exit
+mv ./*.mrpack "/exports" || exit
+
+UPDATE_TARGET="latest_server.mrpack"
+echo "Updating $UPDATE_TARGET";
+cd "/exports" || exit
+rm "$UPDATE_TARGET";
+if [ $? -ne 0 ]; then
+    echo "WARN: Failed to delete the older $UPDATE_TARGET. Not treating it as an error, as it simply must've already not existed. (And if it does exist - the next command will either throw an error and properly exit or simply overwrite it.)";
+fi
+cp "$SERVERPACK" "$UPDATE_TARGET" || exit
+
+echo "DONE! Waiting for 2min until the next cycle.";
+sleep 120
+exec "/app/ci.sh";
+echo "ERROR: If you're seeing this, the next cycle couldn't be started!";
+exit 1;
